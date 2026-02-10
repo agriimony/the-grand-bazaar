279e1a8379057c15ca15495f7f597d69f6d9dcad
agrimony <agriimony@gmail.com>
2026-02-10 08:29:30 +0800

    Render cast-hash route directly instead of redirect

--- app/c/[castHash]/page.js
+++ app/c/[castHash]/page.js
@@ -1,6 +1,13 @@
-import { redirect } from 'next/navigation';
+import AutoFitTitle from '../../../components/AutoFitTitle';
+import BazaarMvpClient from '../../../components/BazaarMvpClient';
 
 export default function CastHashPage({ params }) {
   const castHash = params?.castHash || '';
-  redirect(`/?castHash=${encodeURIComponent(castHash)}`);
+
+  return (
+    <main className="bazaar-shell">
+      <AutoFitTitle text="The Grand Bazaar" />
+      <BazaarMvpClient initialCastHash={castHash} />
+    </main>
+  );
 }
