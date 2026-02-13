e9b6c2874b8a6ca392a216819c019a4c5b488458
agrimony <agriimony@gmail.com>
2026-02-14 00:08:02 +0800

    Hide maker Approve button when selected offer amount exceeds available balance

--- components/BazaarMvpClient.jsx
+++ components/BazaarMvpClient.jsx
@@ -1523,7 +1523,7 @@ export default function BazaarMvpClient({ initialCompressed = '', initialCastHas
               </div>
             ) : makerMode ? (
               <div className="rs-btn-stack">
-                <button className="rs-btn rs-btn-positive" onClick={onMakerApprove}>Approve</button>
+                {!makerSenderInsufficient ? <button className="rs-btn rs-btn-positive" onClick={onMakerApprove}>Approve</button> : null}
                 <button className="rs-btn" onClick={cycleMakerExpiry}>{makerExpiryLabel(makerExpirySec)}</button>
               </div>
             ) : (
